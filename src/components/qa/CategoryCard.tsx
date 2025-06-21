868fb37b048b587157d59cce19ec593a73f71c48
Simon Dodson <simon.dodson.th@gmail.com>
2025-06-21 06:39:18 +0000

    https://answers.cpp41419.com.au/ NOTE STYLING MORE QUISTIONS AND FOOTER

--- src/components/qa/CategoryCard.tsx
+++ src/components/qa/CategoryCard.tsx
@@ -1,3 +1,4 @@
+
 import Link from 'next/link';
 import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from '@/components/ui/card';
 import { ArrowRight, HelpCircle } from 'lucide-react'; 
